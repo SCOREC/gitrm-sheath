c9742cae2b54527fc4967999b5c4c9f8f2846e94
Vignesh Vittal Srinivasaragavan <vickyragav95@gmail.com>
2022-05-22 19:12:23 -0400

    added particle status and rand disp vector

--- src/GitrmSheathTestUtils.hpp
+++ src/GitrmSheathTestUtils.hpp
@@ -11,29 +11,34 @@ private:
     Mesh meshObj_;
     Vector2View positions_;
     IntView elementIDs_;
+    BoolView status_;
 
 public:
     int getTotalParticles();
     Mesh getMeshObj();
     Vector2View getParticlePostions();
     IntView getParticleElementIDs();
+    BoolView getParticleStatus();
+    int computeTotalActiveParticles();
     void validateP2LAlgo();
-
+    void T2LTracking(Vector2View dx);
     Particles(){};
 
     Particles(int numParticles,
               Mesh meshObj,
               Vector2View positions,
-              IntView elementIDs):
+              IntView elementIDs,
+              BoolView status):
               numParticles_(numParticles),
               meshObj_(meshObj),
               positions_(positions),
-              elementIDs_(elementIDs){};
+              elementIDs_(elementIDs),
+              status_(status){};
 
 };
 
 Particles initializeParticles(int numParticles, Mesh meshObj, unsigned int rngSeed);
-
+Vector2View getRandDisplacements(int numParticles, int rngSeed, double scaleFactor);
 
 }
 
