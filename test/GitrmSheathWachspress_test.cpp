0afe46fae2e72c237aa300afb422681a3ccdd404
Vignesh Vittal Srinivasaragavan <vickyragav95@gmail.com>
2022-12-15 22:01:58 -0500

    added rng seed

--- test/GitrmSheathWachspress_test.cpp
+++ test/GitrmSheathWachspress_test.cpp
@@ -1,12 +1,13 @@
 #include "GitrmSheath.hpp"
-
 int main( int argc, char* argv[] ) {
 
     Kokkos::initialize(argc,argv);{
 	//initialaize simple mesh 
         auto mesh = sheath::initializeSimpleMesh();
         // inititalizew single particle
-        auto part = sheath::initializeSingleParticle(mesh,1234);
+	int rngSeed = 1010;
+	// change seed (second argument) for different initial particle location
+        auto part = sheath::initializeSingleParticle(mesh,rngSeed);
 	int npart = part.getTotalParticles();
 
         part.interpolateWachpress();	
