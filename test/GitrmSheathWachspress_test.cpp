17c88fa8eddc41d4dfd2b0824778f06bafb336ef
Cameron Smith <smithc11@rpi.edu>
2023-02-06 14:24:30 -0500

    add netcdf C interface dependency

--- test/GitrmSheathWachspress_test.cpp
+++ test/GitrmSheathWachspress_test.cpp
@@ -1,7 +1,17 @@
 #include "GitrmSheath.hpp"
+#include <netcdf.h>
+
+// from https://docs.unidata.ucar.edu/netcdf-c/current/sfc__pres__temp__rd_8c_source.html
+// Handle errors by printing an error message and exiting with a non-zero status.
+#define ERR(e) {printf("Error: %s\n", nc_strerror(e)); return 2;}
+
 int main( int argc, char* argv[] ) {
 
     Kokkos::initialize(argc,argv);{
+        int retval;
+        int ncid;
+        if ((retval = nc_open(argv[1], NC_NOWRITE, &ncid)))
+          ERR(retval);
 	//initialaize simple mesh 
         auto mesh = sheath::initializeSimpleMesh();
         // inititalizew single particle
